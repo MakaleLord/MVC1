9c9dd0de865a9abd93c79a3d1ea53417b7146e54
Makale Lord <110202582+MakaleLord@users.noreply.github.com>
2022-08-13 14:57:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,30 +9,6 @@ day = ["2", "7", "4"]
 month = ["5", "1" "8"]
 
 year = ["0", "9", "3"]
-####### E X E R C I S E S #########
-#
-#  Exercise 1: Let's start with our first route
-#  Exercise 2: Set the day in past you want to go
-#  Exercise 3: Brace yourself for a trip to past
-#  Exercise 4: Show the past webpage in the flask app.
-#  Bonus: Show the futuristic loader while the iframe is loading. 
-# 
-#  Homework 1: Go to random date when its not mentioned in url.
-#              - Import the random module
-#              - Create a new function named home_page.
-#              - Run this function on route /
-#              - Get three random numbers for day, month and year using random.randint
-#              - Import the redirect, url_for from flask
-#              - Inside home_page()
-#                  Redirect the route to time_machine using redirect
-#                  Send the random day, month and year as data while calling url_for function
-#
-#  Homework 2: Get the site to visit from the route as well. 
-#              - Add another variable for website in route for time_machine
-#              - Add a parameter in time_machine function for website
-#              - Use website parameter instead of site variable while generating api url.
-#              - Set the website route variable to site variable where you saved the website.
-###################################
 
 flux = "http://archive.org/wayback/available?"
 
